430aa581ec598b7c1c2b07e967f57135a893a2af
Phumrapee Limpianchop <git@rayriffy.com>
2019-04-28 17:59:55 +0700

    :sparkles: Custom port to service

--- src/services/sequelize.js
+++ src/services/sequelize.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize'
 import dotenv from 'dotenv'
 
 dotenv.config()
-const {MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB} = process.env
+const {MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB} = process.env
 
 const sequelize = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASS, {
   host: MYSQL_HOST,
+  port: MYSQL_PORT,
   dialect: 'mysql',
 })
 
