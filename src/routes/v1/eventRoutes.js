710856188c065aae74832ec7feec9629c564eff8
Phumrapee Limpianchop <git@rayriffy.com>
2019-04-28 22:16:26 +0700

    :sparkles: Connect all routes

--- src/routes/v1/eventRoutes.js
+++ src/routes/v1/eventRoutes.js
@@ -2,11 +2,15 @@ import express from 'express'
 
 import eventIndex from './event/index'
 import eventCreate from './event/create'
+import eventList from './event/list'
+import eventRemove from './event/remove'
 
 const router = express.Router()
 
 router.use('/', eventIndex)
 
 router.use('/create', eventCreate)
+router.use('/list', eventList)
+router.use('/remove', eventRemove)
 
 export default router
--- src/routes/v1/flightRoutes.js
+++ src/routes/v1/flightRoutes.js
@@ -1,9 +1,20 @@
 import express from 'express'
 
 import flightIndex from './flight/index'
+import flightCreate from './flight/create'
+import flightGet from './flight/get'
+import flightList from './flight/list'
+import flightRemove from './flight/remove'
+import flightReserve from './flight/reserve'
 
 const router = express.Router()
 
 router.use('/', flightIndex)
+
+router.use('/create', flightCreate)
+router.use('/list', flightList)
+router.use('/get', flightGet)
+router.use('/remove', flightRemove)
+router.use('/reserve', flightReserve)
 
 export default router
